ccc55ebf1372f9eef67445d741b93d0343dcc8ec
Ananysharma08 <50516889+Ananysharma08@users.noreply.github.com>
2020-10-05 01:54:14 +0530

    Add files via upload

---
+++ Day-4.cpp
@@ -0,0 +1,25 @@
+ bool compare(const vector<int>&a,const vector<int>&b){
+         if(a[0]==b[0]){
+            return a[1]>b[1];
+        }
+        return a[0]<b[0];
+    }
+
+
+class Solution {
+public:
+    
+    
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end(),compare);
+        int ans=0;
+        int right=INT_MIN;
+        for(int i = 0; i < intervals.size(); i++)
+           if(intervals[i][1] > right){
+               ans++,
+               right = intervals[i][1];
+           }
+        return ans;
+        
+    }
+};
